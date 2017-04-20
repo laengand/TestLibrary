0d434dee19c5ece6017a84a338ea4d6a23ad49d4
Lasse Andersen (LAAD) <laad@dgs.com>
2017-04-20 13:41:09 +0200

    Added more comments

--- Matlab/Test.m
+++ Matlab/Test.m
@@ -1,6 +1,6 @@
 clear
 clc
-
+%% Init Testlibrary
 [folder, name, ext] = fileparts(mfilename('fullpath'));
 cd(folder);
 
@@ -15,37 +15,40 @@ LibraryName = 'TestLibrary.dll';
 % Load the TestLibrary 
 try
    asmInfo = NET.addAssembly([PathToLibrary LibraryName]);
+   import TestLibrary.*;
 catch ex
     ex.ExceptionObject.LoaderExceptions.Get(0).Message
     error('Library or support file loading problem. Script halted')
 end
 
-% Set the pid 
-hexId = '0012';
+%% Set the pid 
+hexId = '0012'; % Replace this with the desired PID
 id = hex2dec(hexId);
 pidFolderPath = 'C:\Users\laad\Documents\Visual Studio 2015\Projects\FirmwareTestTool\PC\code\Output\Debug\Command Definitions';
 pidFilePath = [pidFolderPath '\USB, PID ' hexId '.txt'];
 
-import TestLibrary.*;
+%% Create test class 
 test = TestClass(id, pidFilePath);
 device = test.GeneratedClass;
 
-% Create default event handlers. These should be modified to correspond to
-% a desired action, when an event is received
-CreateEventHandlers(id,test);
-
-% Create instance of event handlers and set up listeners
-eventHandlers = EventHandlerClass0x0012();
-e = cell(test.eventHandlerNames)';
+%% Create default event handlers.
+% These should be modified to correspond to a desired action, when an event is received
+if exist(['EventHandlerClass0x' hexId '.m'],'file') == 0
+    CreateEventHandlers(id,test);
+    error('No event handlers existed. A default eventhandler class has been created')
+end
+    
+%% Create instance of event handlers and set up listeners
+eventHandlers = EventHandlerClass0x0012(); % EventHandlerClass0x**** replace this with the desired PID
 els = eventHandlers.SetupEventListeners(device);
 
-% Connect to the device
+%% Connect to the device
 if test.Connect() == 0
     error('Unable to connect to device. Reset the device.')
 end
     
 
-% Perform test
+%% Perform test
 disp('Test is running. Hit a key to stop')
 device.Ping
     
@@ -53,5 +56,5 @@ pause('on')
 pause
 pause('off')
 
-% Remember to disconnect from the device
+%% Disconnect from the device
 test.Disconnect();
