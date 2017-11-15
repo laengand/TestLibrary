0bb63ffeec3971761509d9b221b183b1dd712ed1
Lasse Andersen (LAAD) <laad@dgs.com>
2017-11-15 12:39:48 +0100

    Add white noise test

---
+++ Matlab/Test/WhiteNoiseTest.m
@@ -0,0 +1,22 @@
+function [tf, chList, fig] = WhiteNoiseTest (fLow, fHigh, x, y, figureVisibleOption)
+    tf = false;
+    chList.level = false;
+    
+    xStartIdx = find(x <= fLow, 1, 'last');
+    xEndIdx = find(x >= fHigh, 1, 'first');
+    
+    average = mean(y(xStartIdx:xEndIdx));
+    fig = figure('Visible', figureVisibleOption);
+    lineFft = semilogx(x,y);
+    
+    line([x(xStartIdx) x(xEndIdx)], [average average] + 3, 'Color','r');
+    line([x(xStartIdx) x(xEndIdx)], [average average] - 3, 'Color','r');
+    
+    if(abs(y(xStartIdx:xEndIdx) - average) <= 3)
+        chList.level = true;
+        tf = true;
+    end
+    
+    
+end
+
