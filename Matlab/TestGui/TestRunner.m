6c14103d30597e53037b6a9a4d70e6921972cb88
Lasse Andersen (LAAD) <laad@dgs.com>
2017-12-19 15:49:49 +0100

    - Compress the output of exceptions to a more readable form

--- Matlab/TestGui/TestRunner.m
+++ Matlab/TestGui/TestRunner.m
@@ -59,10 +59,15 @@ classdef TestRunner < handle
                         self.StopTest;
                 end
             catch ex
+                disp('--')
+                disp('Error:')
                 disp(ex.message)
+                disp('--')
+                disp('Stack trace:')
                 for k=1:length(ex.stack)
-                    ex.stack(k)
+                    disp([ex.stack(k).name ' Line: <a href="matlab: matlab.desktop.editor.openAndGoToLine(' '''' which(ex.stack(k).file) '''' ',' num2str(ex.stack(k).line) ');">' num2str(ex.stack(k).line) '</a>']);
                 end
+                disp('--')
             end
         end
         %% UI callbacks        
