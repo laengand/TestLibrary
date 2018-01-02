b2e9724ee0d0ae17844ef08f08732c5d87b31fe3
Lasse Andersen (LAAD) <laad@dgs.com>
2018-01-02 14:58:13 +0100

    - Add function description

--- Matlab/Test/FindLevel.m
+++ Matlab/Test/FindLevel.m
@@ -1,28 +1,45 @@
 function xLevelList = FindLevel(x, y, yLevel)
-    % FindLevel
-    %     Find the
-%         fs = 10;
-%         x = 1:1/fs:fs;
-%         y = 5*sin(2*pi*x);
-%         ylevel = 2;
-    
+    %% Find Level
+    % FindLevel(x, y, yLevel) finds the x-value(xLevel) corresponding to 
+    % yLevel in the dataset given by x,y. The xLevel is found by fitting a 
+    % 2nd order polynomial to the 4 dataset values closest to the 
+    % (xLevel,yLevel) dataset. If more than 1 xLevel corresponds to the 
+    % yLevel, then a vector of xLevels is returned.
+    %
+    %        y
+    %        ^
+    %        |
+    %        |             (x4,y4)*
+    %        |                   /
+    %        |                  /
+    %        |                 / 
+    %        |         (x3,y3)* 
+    %        |               /  
+    % yLevel |- - - - - - - /     
+    %        |             /|
+    %        |     (x2,y2)* |
+    %        |           /  |
+    %        |          /   |
+    %        |         /    |
+    %        | (x1,y1)*     |
+    %        |              |
+    %        |              |
+    %        ----------------------------------------------> x
+    %                       xLevel
     xLevelList = [];
     idx = y < yLevel;
     
     for i = 1:length(idx)-1
         if(idx(i) ~= idx(i+1))
-            
-            %% polyfit
             xPol = x(i-1:i+2);
             yPol = y(i-1:i+2);
-            pfit = polyfit(xPol,yPol,3);
+            pfit = polyfit(xPol,yPol,2);
             % y = ax^2 + bx + c
             % pfit = [a b c]
-            % solve for x 
+            % solve 0 = ax^2 + bx + c-y for x
             xRoots = roots([pfit(1:end-1) pfit(end)-yLevel]);
-            xLevel = xRoots(x(i)< xRoots & xRoots < x(i+1));
+            xLevel = xRoots(x(i) < xRoots & xRoots < x(i+1));
             xLevelList = [xLevelList xLevel]; %#ok
-
         end
     end
 end
